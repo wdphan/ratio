54c2738e1acde79f157e21c921a6dc61458e45c2
William Phan <williamphan@MacBook-Pro-40.fios-router.home>
2022-12-21 02:19:41 -0500

    bg css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,14 +8,14 @@ const Header: FC = () => {
 	return (
 		<header aria-label="Site Header" className="bg-opacity-0	">
 			<div className="mx-auto flex h-16 max-w-screen-xl items-center gap-8 px-4 sm:px-6 lg:px-8">
-				<a className="block text-teal-600 dark:text-teal-300">
+				<a className="block pt-2">
 					<span className="sr-only">Home</span>
 					<Image width={40} height={40} src={logo} alt="logo" />
 				</a>
 
 				<div className="flex flex-1 items-center justify-end md:justify-between">
 					<nav aria-label="Site Nav" className="hidden md:block">
-						<ul className="flex items-center gap-6 font-Syne text-base">
+						<ul className="flex items-center gap-6 font-Syne text-xl">
 							<li>
 								<a className=" transition hover:text-gray-500/75  text-white ">Fractionalize</a>
 							</li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,16 @@
 import { FC, useState } from 'react'
 import { APP_NAME } from '@/lib/consts'
-import ConnectWallet from '@/components/ConnectWallet'
-import { BookOpenIcon, CodeIcon, ShareIcon } from '@heroicons/react/outline'
 import logo from 'images/logo.png'
 import Image from 'next/image'
 // import Header from '@/components/Header'
 import Link from 'next/link'
 import Header from '@/components/Header'
+import styles from 'src/styles/app.module.css'
 
 const Home: FC = () => {
 	return (
-		<div className="bg-gradient-to-br from-[#7B00A0] via-[#AB42CB] to-[#FFFDFF]">
+		// bg-gradient-to-br from-[#7B00A0] via-[#AB42CB] to-[#FFFDFF]
+		<div className={styles.background}>
 			<section className="text-white body-font flex items-center justify-center min-h-screen  pt-32 sm:pt-38 ">
 				<div className="absolute w-full top-3">
 					{' '}
@@ -19,11 +19,13 @@ const Home: FC = () => {
 				<div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col ">
 					<Image width={50} height={50} src={logo} alt="logo" className="animate-spin-slow" />
 					<div className="text-center lg:w-2/3 w-full flex flex-col items-center justify-center">
-						<p className="pt-8 font-Syne text-lg ">A Mini-Implementation of Fractional Protocol</p>
+						<p className="pt-8 font-Syne text-lg ">
+							A Mini-Implementation of <a className="decoration underline">@Fractional</a> Protocol
+						</p>
 						<h1 className="flex flex-row content-center items-center justify-center pt-[-1rem] sm:mt-[-1.5rem] text-[6rem] sm:text-[10rem] tracking-tighter font-normal font-Audio">
 							RATIO
 						</h1>
-						<p className="max-w-lg pt-[-1rem] sm:mt-[-1.5rem] pb-8 text-lg font-Syne">
+						<p className="max-w-2xl pt-[-1rem] sm:mt-[-1.5rem] pb-8 text-lg font-Syne">
 							Hoomanize is a digital agency and developerment studio. We are helping company to create
 							product and platform that humanize the human
 						</p>
@@ -31,7 +33,7 @@ const Home: FC = () => {
 							<button
 								className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded-tl-lg
 
-text-lg"
+text-lg font-Syne font-semibold"
 							>
 								Button
 							</button>
